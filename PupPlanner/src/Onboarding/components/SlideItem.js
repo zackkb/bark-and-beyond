61feef3e4e4c83dd45d4465c2efbe26f6fe37b62
zackkb <98679473+zackkb@users.noreply.github.com>
2023-04-23 22:32:29 -0500

    Adding login/signup, and stylings

--- PupPlanner/src/Onboarding/components/SlideItem.js
+++ PupPlanner/src/Onboarding/components/SlideItem.js
@@ -1,9 +1,19 @@
-import { Image, StyleSheet, Text, View, Dimensions } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const { width, height } = Dimensions.get("screen");
 
-const SlideItem = ({ item }) => {
+const SlideItem = ({ item, index, lastIndex }) => {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <Image source={item.img} resizeMode="contain" style={styles.image} />
@@ -11,6 +21,24 @@ const SlideItem = ({ item }) => {
       <View style={styles.content}>
         <Text style={styles.title}>{item.title}</Text>
         <Text style={styles.description}>{item.description}</Text>
+
+        {index === lastIndex && (
+          <View style={styles.buttonsContainer}>
+            <TouchableOpacity
+              style={styles.signupButton}
+              onPress={() => navigation.navigate("SignUp")}
+            >
+              <Text style={styles.buttonText}>Sign Up</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.loginButton}
+              onPress={() => navigation.navigate("Login")}
+            >
+              <Text style={[styles.buttonText, { color: "#000" }]}>Log In</Text>
+            </TouchableOpacity>
+          </View>
+        )}
       </View>
     </View>
   );
@@ -39,11 +67,47 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "bold",
     color: "#333",
+    marginBottom: 10,
   },
   description: {
     fontSize: 16,
     fontWeight: 400,
-    lineHeight: 24,
+    lineHeight: 19.36,
     color: "#333",
+    textAlign: "center",
+    flexWrap: "wrap",
+  },
+  buttonsContainer: {
+    marginTop: 20,
+    alignItems: "center",
+  },
+
+  signupButton: {
+    height: 48,
+    width: 345,
+    borderRadius: 30,
+    padding: 12,
+    backgroundColor: "#323841",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  loginButton: {
+    height: 48,
+    width: 345,
+    borderRadius: 30,
+    padding: 12,
+    backgroundColor: "#FFF",
+    borderWidth: 2,
+    borderColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+
+  buttonText: {
+    color: "#FFF",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
