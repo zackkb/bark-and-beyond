e43351edd2d7a90242a50e67fed675c09a477bdd
zackkb <98679473+zackkb@users.noreply.github.com>
2023-07-17 19:21:34 -0500

    Adding scroll view and other shit to play date man i'm tired
    yeah brett here's some shit man not much else lol

--- PupPlanner/src/Dashboard/components/Playdate.js
+++ PupPlanner/src/Dashboard/components/Playdate.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import ScreenWrapper from "../../ScreenWrapper";
 
 const Playdate = ({ navigation }) => {
@@ -57,7 +64,7 @@ const Playdate = ({ navigation }) => {
           </Text>
         </TouchableOpacity>
       </View>
-      <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.container}>
         <Image
           style={styles.dogImage}
           source={require("../assets/max_dog.png")}
@@ -74,18 +81,23 @@ const Playdate = ({ navigation }) => {
           </View>
 
           {isExpanded && (
-            <Text style={styles.normalText}>
-              {" "}
-              Meet Max, a friendly and energetic Golden Retriever who loves to
-              play and socialize with other dogs. Max's owner is hoping to set
-              up a play date with other dog owners in the area, as Max always
-              has a blast playing with his furry friends. With his wagging tail
-              and playful demeanor, Max is sure to bring a smile to anyone's
-              face and make new doggie friends in no time!
-            </Text>
+            <View>
+              <Text style={styles.normalText}>
+                {" "}
+                Meet Max, a friendly and energetic Golden Retriever who loves to
+                play and socialize with other dogs. Max's owner is hoping to set
+                up a play date with other dog owners in the area, as Max always
+                has a blast playing with his furry friends. With his wagging
+                tail and playful demeanor, Max is sure to bring a smile to
+                anyone's face and make new doggie friends in no time!
+              </Text>
+              <TouchableOpacity style={styles.messageButton}>
+                <Text style={styles.buttonText}>Message</Text>
+              </TouchableOpacity>
+            </View>
           )}
         </TouchableOpacity>
-      </View>
+      </ScrollView>
     </ScreenWrapper>
   );
 };
@@ -94,7 +106,6 @@ export default Playdate;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: "center",
     padding: 10,
   },
@@ -129,5 +140,19 @@ const styles = StyleSheet.create({
   },
   groupTextContainer: {
     alignItems: "center",
+  },
+  messageButton: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+    width: "45%",
+    padding: 12,
+    borderRadius: 30,
+    backgroundColor: "#323841",
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "#FFFFFF",
   },
 });
