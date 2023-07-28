b2f9765a14abc4c3df5848ca445f886122d5f89a
zackkb <98679473+zackkb@users.noreply.github.com>
2023-07-27 19:33:43 -0500

    playdate is done mfers
    hell yeah i don't know what to say here

--- PupPlanner/src/Dashboard/components/Playdate.js
+++ PupPlanner/src/Dashboard/components/Playdate.js
@@ -1,21 +1,26 @@
 import React, { useState } from "react";
 import {
-  StyleSheet,
-  Text,
   View,
+  Text,
   Image,
   TouchableOpacity,
   ScrollView,
+  StyleSheet,
+  Dimensions,
 } from "react-native";
 import ScreenWrapper from "../../ScreenWrapper";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const Playdate = ({ navigation }) => {
   const [isExpanded, setIsExpanded] = useState(false);
   const [selectedSection, setSelectedSection] = useState("Play Date");
+  const [isLiked, setIsLiked] = useState(false);
 
   const handlePress = () => {
     setIsExpanded(!isExpanded);
   };
+
+  const deviceWidth = Dimensions.get("window").width;
 
   return (
     <ScreenWrapper navigation={navigation}>
@@ -65,10 +70,22 @@ const Playdate = ({ navigation }) => {
         </TouchableOpacity>
       </View>
       <ScrollView contentContainerStyle={styles.container}>
-        <Image
-          style={styles.dogImage}
-          source={require("../assets/max_dog.png")}
-        />
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.dogImage}
+            source={require("../assets/max_dog.png")}
+          />
+          <TouchableOpacity style={styles.closeButton} onPress={() => {}}>
+            <Icon name="times-circle" size={30} color="white" />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.heartButton}
+            onPress={() => setIsLiked(!isLiked)}
+          >
+            <Icon name="heart" size={30} color={isLiked ? "red" : "white"} />
+          </TouchableOpacity>
+        </View>
 
         <TouchableOpacity onPress={handlePress}>
           <Text style={styles.boldText}>Max, 1 year old, 50lbs</Text>
@@ -77,26 +94,56 @@ const Playdate = ({ navigation }) => {
           </Text>
           <View style={styles.groupTextContainer}>
             <Text style={styles.normalText}>Group or 1:1 play</Text>
-            {!isExpanded && <View style={styles.line} />}
+            {!isExpanded && <Icon name="chevron-down" size={16} />}
+            {isExpanded && <Icon name="chevron-up" size={16} />}
           </View>
-
-          {isExpanded && (
-            <View>
-              <Text style={styles.normalText}>
-                {" "}
-                Meet Max, a friendly and energetic Golden Retriever who loves to
-                play and socialize with other dogs. Max's owner is hoping to set
-                up a play date with other dog owners in the area, as Max always
-                has a blast playing with his furry friends. With his wagging
-                tail and playful demeanor, Max is sure to bring a smile to
-                anyone's face and make new doggie friends in no time!
-              </Text>
-              <TouchableOpacity style={styles.messageButton}>
-                <Text style={styles.buttonText}>Message</Text>
-              </TouchableOpacity>
-            </View>
-          )}
         </TouchableOpacity>
+
+        {isExpanded && (
+          <View>
+            <Text style={styles.expandedText}>
+              Can play for long periods of time and loves to fetch. Gets along
+              with every dog and person he meets.
+            </Text>
+          </View>
+        )}
+
+        <View style={styles.separator} />
+
+        <View style={styles.localContainer}>
+          <Text style={styles.title}>Local to you</Text>
+          <View style={styles.userCard}>
+            <View style={styles.profileContainer}>
+              <Image
+                source={require("../assets/snoopy.png")}
+                style={styles.userImage}
+              />
+              <Text style={styles.userName}>Snoopy</Text>
+            </View>
+            <View style={styles.formContainer}>
+              <View style={styles.textAndIconContainer}>
+                <Text style={styles.userInfoBold}>2 years old, 25lbs</Text>
+                <Text style={styles.userInfo}>Group Play</Text>
+              </View>
+            </View>
+          </View>
+          {/* New profile for 'Ruff' */}
+          <View style={styles.userCard}>
+            <View style={styles.profileContainer}>
+              <Image
+                source={require("../assets/ruff.png")}
+                style={styles.userImage}
+              />
+              <Text style={styles.userName}>Ruff</Text>
+            </View>
+            <View style={styles.formContainer}>
+              <View style={styles.textAndIconContainer}>
+                <Text style={styles.userInfoBold}>3 years old</Text>
+                <Text style={styles.userInfo}>1:1 Play</Text>
+              </View>
+            </View>
+          </View>
+        </View>
       </ScrollView>
     </ScreenWrapper>
   );
@@ -116,9 +163,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#B8DFA9",
   },
   dogImage: {
-    width: "100%",
-    height: 200,
-    resizeMode: "contain",
+    width: "90%",
+    aspectRatio: 361 / 240,
+    resizeMode: "cover",
+    borderRadius: 8,
+    flexShrink: 0,
     marginBottom: 20,
   },
   boldText: {
@@ -131,12 +180,6 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 16,
     marginBottom: 10,
-  },
-  line: {
-    borderBottomColor: "grey",
-    borderBottomWidth: 1,
-    width: "100%",
-    marginVertical: 10,
   },
   groupTextContainer: {
     alignItems: "center",
@@ -154,5 +197,73 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     color: "#FFFFFF",
+  },
+  imageContainer: {
+    position: "relative",
+    alignItems: "center",
+    width: "100%",
+  },
+  closeButton: {
+    position: "absolute",
+    bottom: "50%",
+    left: 20,
+    padding: 10,
+    transform: [{ translateY: 15 }],
+  },
+  heartButton: {
+    position: "absolute",
+    bottom: "50%",
+    right: 20,
+    padding: 10,
+    transform: [{ translateY: 15 }],
+  },
+  titleContainer: {
+    marginTop: 20,
+    marginLeft: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  userCard: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    padding: 10,
+    marginBottom: 10,
+  },
+  profileContainer: {
+    marginRight: 10,
+    alignItems: "flex-start",
+  },
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginBottom: 5,
+  },
+  userName: {
+    fontSize: 14,
+    textAlign: "left",
+  },
+  formContainer: {
+    width: "84%",
+    height: 73,
+    flexShrink: 0,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: "#E9EEF6",
+  },
+  userInfo: {
+    fontSize: 16,
+  },
+  userInfoBold: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  textAndIconContainer: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
   },
 });
